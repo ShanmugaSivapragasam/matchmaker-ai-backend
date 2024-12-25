a761f3817bbd54012fa999fa1a450caf758856be
ShanmugaSivapragasam <shanmuga.sivapragasam@gmail.com>
2024-12-24 20:39:01 -0500

    spring intitialzr working set up

---
+++ src/test/java/io/sharpen/matchmaker_ai_backend/MatchmakerAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sharpen.matchmaker_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmakerAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
