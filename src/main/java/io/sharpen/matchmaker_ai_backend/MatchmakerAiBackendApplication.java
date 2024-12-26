c94b2c7e123301e953ed1d78e4c93e1b6f432793
ShanmugaSivapragasam <shanmuga.sivapragasam@gmail.com>
2024-12-25 21:37:04 -0500

    Profile is persisted and fetched from local mongo repository.

--- src/main/java/io/sharpen/matchmaker_ai_backend/MatchmakerAiBackendApplication.java
+++ src/main/java/io/sharpen/matchmaker_ai_backend/MatchmakerAiBackendApplication.java
@@ -1,13 +1,37 @@
 package io.sharpen.matchmaker_ai_backend;
 
+import io.sharpen.matchmaker_ai_backend.profiles.Gender;
+import io.sharpen.matchmaker_ai_backend.profiles.Profile;
+import io.sharpen.matchmaker_ai_backend.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatchmakerAiBackendApplication {
+public class MatchmakerAiBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchmakerAiBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Profile shanProfile = new Profile(
+				"1",
+				"Shan",
+				"S",
+				88,
+				"Indian",
+				Gender.MALE,
+				"Student",
+				"shan.jpg",
+				"INT"
+		);
+		profileRepository.save(shanProfile);
+		profileRepository.findAll().forEach(System.out::println);
+	}
 }
---
+++ src/main/java/io/sharpen/matchmaker_ai_backend/profiles/Gender.java
@@ -0,0 +1,7 @@
+package io.sharpen.matchmaker_ai_backend.profiles;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    NON_BINARAY
+}
